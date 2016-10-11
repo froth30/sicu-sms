211519a0ea6df1dd768fc90dce84d30fafc364cb
teddybear02 <theo.frohlich@gmail.com>
2016-10-11 15:14:24 -0400

    Revised package structure and header doc

---
+++ src/main/arduino/MasterSlave.ino
@@ -0,0 +1,14 @@
+/*                                                                      *\
+**                    SICU Stress Measurement System                    **
+**                      Project P04  |  C380 Team A                     **
+**          EBME 380: Biomedical Engineering Design Experience          **
+**                    Case Western Reserve University                   **
+**                          2016 Fall Semester                          **
+\*                                                                      */
+void setup() {
+
+}
+
+void loop() {
+
+}
