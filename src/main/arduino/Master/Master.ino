6678cb43247b9c36874a8adb6ac1eedd7b69009e
teddybear02 <theo.frohlich@gmail.com>
2016-10-13 18:46:26 -0400

    Establish that Arduino boards can communicate with each other

--- src/main/arduino/Master/Master.ino
+++ src/main/arduino/Master/Master.ino
@@ -6,10 +6,24 @@
 **                          2016 Fall Semester                          **
 \*                                                                      */
 
-void setup() {
+const byte toSlave = 2;
 
+long cmdCount = 0;
+long count = 0;
+
+void setup() {
+  pinMode(toSlave, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-
+  count++;
+  if (count >= 2^64 - 1) {
+    count = 0;
+    digitalWrite(toSlave, HIGH);
+    Serial.print(++cmdCount);
+    Serial.println(" Sent command to slave.");
+  } else {
+    digitalWrite(toSlave, LOW);
+  }
 }
--- src/main/arduino/Slave/Slave.ino
+++ src/main/arduino/Slave/Slave.ino
@@ -6,10 +6,30 @@
 **                          2016 Fall Semester                          **
 \*                                                                      */
 
-void setup() {
+const byte fromMaster = 22;
+const byte LED = 49;
 
+int count;
+
+byte incomingByte = 0;
+
+void setup() {
+  pinMode(fromMaster, INPUT);
+  pinMode(LED, OUTPUT);
+  count = 0;
+  Serial.begin(9600);
 }
 
 void loop() {
-
+  if (digitalRead(fromMaster) == HIGH) {
+    Serial.println("Received command from master.");
+    count = 10;
+  }
+  if (count > 0) {
+    count--;
+    digitalWrite(LED, HIGH);
+  } else {
+    count = 0;
+    digitalWrite(LED, LOW);
+  }
 }
