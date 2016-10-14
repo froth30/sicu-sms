7bfc91631e2bb6c5a48a18a1f6edf1fae49c29d6
teddybear02 <theo.frohlich@gmail.com>
2016-10-14 01:51:52 -0400

    Add header doc

--- src/main/java/edu/cwru/sicu_sms/Controller.java
+++ src/main/java/edu/cwru/sicu_sms/Controller.java
@@ -1,7 +1,22 @@
+/*                                                                      *\
+**                    SICU Stress Measurement System                    **
+**                      Project P04  |  C380 Team A                     **
+**          EBME 380: Biomedical Engineering Design Experience          **
+**                    Case Western Reserve University                   **
+**                          2016 Fall Semester                          **
+\*                                                                      */
+
 package edu.cwru.sicu_sms;
 
 /**
- * Created by theof_000 on 10/14/2016.
+ * The controller for the front-end program.
+ *
+ * @since October 13, 2016
+ * @author Ted Frohlich <ttf10@case.edu>
+ * @author Abby Walker <amw138@case.edu>
  */
 public class Controller {
+    
+    
+    
 }
